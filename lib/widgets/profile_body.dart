de9404af9e20bcffb9243295e8682c01e85cb2ab
kimjongseong <miaer456@naver.com>
2022-02-14 00:46:36 +0900

    Instagram 2

--- lib/widgets/profile_body.dart
+++ lib/widgets/profile_body.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_project/constants/common_size.dart';
 import 'package:flutter_project/constants/screen_size.dart';
@@ -16,60 +17,81 @@ class _ProfileBodyState extends State<ProfileBody> {
       child: CustomScrollView(
         slivers: [
           SliverList(
-              delegate: SliverChildListDelegate([
-            _username(),
-            _userBio(),
-            _editProfileBtn(),
-            _tabButtons(), 
-                _selectedIndicator(),
-          ]))
+            delegate: SliverChildListDelegate([
+              _username(),
+              _userBio(),
+              _editProfileBtn(),
+              _tabButtons(),
+              _selectedIndicator(),
+            ]),
+          ),
+          SliverToBoxAdapter(
+              child: GridView.count(
+            physics: NeverScrollableScrollPhysics(),
+            shrinkWrap: true,
+            crossAxisCount: 3,
+            childAspectRatio: 1,
+            children: List.generate(
+                30,
+                (index) => CachedNetworkImage(
+                  fit: BoxFit.cover,
+                      imageUrl: "https://picsum.photos/id/$index/100/100",
+                    )),
+          ))
         ],
       ),
     );
   }
 
-  Widget _selectedIndicator(){
+  Widget _selectedIndicator() {
     return AnimatedContainer(
       duration: Duration(milliseconds: 200),
-      alignment: _selectedTap==SelectedTap.left?Alignment.centerLeft:Alignment.centerRight,
+      alignment: _selectedTap == SelectedTap.left
+          ? Alignment.centerLeft
+          : Alignment.centerRight,
       child: Container(
         height: 3,
-        width: size.width/2, color: Colors.black87,
+        width: size.width / 2,
+        color: Colors.black87,
       ),
       curve: Curves.easeInOut,
     );
   }
-  
+
   Row _tabButtons() {
     return Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Expanded(
-                child: IconButton(
-                    icon: ImageIcon(
-                      AssetImage('assets/images/grid.png'),
-                      color: _selectedTap==SelectedTap.left ? Colors.black : Colors.black26,
-                    ),
-                    onPressed: () {
-                      setState(() {
-                        _selectedTap = SelectedTap.left;
-                      });
-                    }),
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        Expanded(
+          child: IconButton(
+              icon: ImageIcon(
+                AssetImage('assets/images/grid.png'),
+                color: _selectedTap == SelectedTap.left
+                    ? Colors.black
+                    : Colors.black26,
               ),
-              Expanded(
-                child: IconButton(
-                    icon: ImageIcon(
-                      AssetImage('assets/images/saved.png'),
-                      color: _selectedTap==SelectedTap.left? Colors.black26 : Colors.black,
-                    ),
-                    onPressed: () {
-                      setState(() {
-                        _selectedTap = SelectedTap.right;
-                      });
-                    }),
-              )
-            ],
-          );
+              onPressed: () {
+                setState(() {
+                  _selectedTap = SelectedTap.left;
+                });
+              }),
+        ),
+        Expanded(
+          child: IconButton(
+              icon: ImageIcon(
+                AssetImage('assets/images/saved.png'),
+                color: _selectedTap == SelectedTap.left
+                    ? Colors.black26
+                    : Colors.black,
+              ),
+              onPressed: () {
+                setState(() {
+                  _selectedTap = SelectedTap.right;
+                });
+              }),
+        )
+      ],
+    );
   }
 
   Padding _editProfileBtn() {
@@ -115,4 +137,4 @@ class _ProfileBodyState extends State<ProfileBody> {
   }
 }
 
-enum SelectedTap{left,right}
+enum SelectedTap { left, right }
