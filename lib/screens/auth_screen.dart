3a714aced58794d79d83521a140d5a603e47f0a1
kimjongseong <miaer456@naver.com>
2022-02-16 23:35:11 +0900

    Instagram 3

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -10,10 +10,7 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
-
-
-  int selectedForm =0;
-
+  int selectedForm = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -24,18 +21,37 @@ class _AuthScreenState extends State<AuthScreen> {
             FadeStack(
               selectedForm: selectedForm,
             ),
-            Container(
-              child: FlatButton(
-                onPressed: () {
-                  setState(() {
-                    if(selectedForm == 0){
-                      selectedForm =1;
-                    }else{
-                      selectedForm=0;
-                    }
-                  });
-                },
-                child: Text('go to Sign up'),
+            Positioned(
+              left: 0,
+              right: 0,
+              bottom: 0,
+              height: 40,
+              child: Container(
+                color: Colors.white,
+                child: FlatButton(
+                  shape: Border(top: BorderSide(color: Colors.grey)),
+                    onPressed: () {
+                      setState(() {
+                        if (selectedForm == 0) {
+                          selectedForm = 1;
+                        } else {
+                          selectedForm = 0;
+                        }
+                      });
+                    },
+                    child: RichText(
+                        text: TextSpan(
+                            text: (selectedForm == 0)
+                                ? "Already have an account"
+                                : "Don't have an account? ",
+                            style: TextStyle(color: Colors.grey),
+                            children: [
+                          TextSpan(
+                              text: (selectedForm == 0) ? "Sign In" : "Sign Up",
+                              style: TextStyle(
+                                  color: Colors.blue,
+                                  fontWeight: FontWeight.bold))
+                        ]))),
               ),
             ),
           ],
