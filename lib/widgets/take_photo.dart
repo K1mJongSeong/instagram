ece6de75928fb125bd7262708f5ada79e15d1235
kimjongseong <miaer456@naver.com>
2022-03-09 21:45:32 +0900

    Instagram 5

--- lib/widgets/take_photo.dart
+++ lib/widgets/take_photo.dart
@@ -1,7 +1,10 @@
+import 'dart:io';
+
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_project/constants/screen_size.dart';
 import 'package:flutter_project/models/camera_state.dart';
+import 'package:flutter_project/screens/share_post_screen.dart';
 import 'package:flutter_project/widgets/my_progress_indicator.dart';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
@@ -35,7 +38,7 @@ class _TakePhotoState extends State<TakePhoto> {
               child: OutlineButton(
                 onPressed: () {
                   if(cameraState.isReadyToTakePhoto){
-                    _attemptTakePhoto(cameraState);
+                    _attemptTakePhoto(cameraState,context);
                   }
                 },
                 shape: CircleBorder(),
@@ -62,11 +65,13 @@ class _TakePhotoState extends State<TakePhoto> {
     );
   }
 
-  void _attemptTakePhoto(CameraState cameraState) async{
+  void _attemptTakePhoto(CameraState cameraState,BuildContext context) async{
     final String timeInMilli = DateTime.now().microsecondsSinceEpoch.toString();
     try{
      final path = join((await getTemporaryDirectory()).path, '$timeInMilli.png');
     await cameraState.controller.takePicture(path);
+    File imageFile=File(path);
+    Navigator.of(context).push(MaterialPageRoute(builder: (_)=>SharePostScreen(imageFile)));
     } catch(e){
 
     }
