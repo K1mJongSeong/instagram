b9204f36d2b9bf95965f37b6c3edac04fab057cb
kimjongseong <miaer456@naver.com>
2022-03-14 23:50:59 +0900

    Instagram 6

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -8,30 +8,49 @@ class SearchScreen extends StatefulWidget {
 }
 
 class _SearchScreenState extends State<SearchScreen> {
+  List<bool> following = List.generate(30, (index) => false);
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: ListView.separated(
           itemBuilder: (context, index) {
             return ListTile(
+              onTap: () {
+                if (following[index]) {
+                  setState(() {
+                    following[index] = !following[index];
+                  });
+                }
+              },
               leading: RoundedAvatar(),
               title: Text('username $index'),
               subtitle: Text('user bio number $index'),
               trailing: Container(
-                height: 30, width: 80,alignment: Alignment.center, decoration: BoxDecoration(
-                color: Colors.red[50],
-                border: Border.all(color: Colors.red,width: 0.5),
-                borderRadius: BorderRadius.circular(8),
-              ),
-                child: Text('fallowing',style: TextStyle(fontWeight: FontWeight.bold),
-                textAlign: TextAlign.center,),
+                height: 30,
+                width: 80,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  color: following[index] ? Colors.red[50] : Colors.blue[50],
+                  border: Border.all(
+                      color: following[index] ? Colors.red : Colors.blue,
+                      width: 0.5),
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: Text(
+                  'fallowing',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.center,
+                ),
               ),
             );
           },
           separatorBuilder: (context, index) {
-            return Divider(color: Colors.grey,);
+            return Divider(
+              color: Colors.grey,
+            );
           },
-          itemCount: 30),
+          itemCount: following.length),
     );
   }
 }
