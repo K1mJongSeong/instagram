140dca147bef3e88ded56ee6d49a286a21ff92ca
kimjongseong <miaer456@naver.com>
2022-04-13 17:27:58 +0900

    Instagram 9

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -3,17 +3,16 @@ import 'package:flutter_project/constants/screen_size.dart';
 import 'package:flutter_project/widgets/profile_body.dart';
 import 'package:flutter_project/widgets/profile_side_menu.dart';
 
-const duration = Duration(milliseconds:300);
+const duration = Duration(milliseconds: 300);
 
 class ProfileScreen extends StatefulWidget {
-
   @override
   _ProfileScreenState createState() => _ProfileScreenState();
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
   final duration = Duration(milliseconds: 300);
-  final menuWidth = size.width/3*2;
+  final menuWidth = size.width / 3 * 2;
 
   MenuStatus _menuStatus = MenuStatus.closed;
   double bodyXPos = 0;
@@ -22,38 +21,39 @@ class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[100],
-      body: Stack(
-        children: [
-        AnimatedContainer(duration: duration,
-        curve: Curves.fastOutSlowIn,
-        child: ProfileBody(onMenuChanged: () {
-          setState(() {
-            _menuStatus = (_menuStatus == MenuStatus.closed)
-                ? MenuStatus.opened
-                : MenuStatus.closed;
+        backgroundColor: Colors.grey[100],
+        body: Stack(
+          children: [
+            AnimatedContainer(
+              duration: duration,
+              curve: Curves.fastOutSlowIn,
+              child: ProfileBody(onMenuChanged: () {
+                setState(() {
+                  _menuStatus = (_menuStatus == MenuStatus.closed)
+                      ? MenuStatus.opened
+                      : MenuStatus.closed;
 
-            switch (_menuStatus) {
-              case MenuStatus.opened:
-                bodyXPos = -menuWidth;
-                menuXPos = size.width -menuWidth;
-                break;
-              case MenuStatus.closed:
-                bodyXPos = 0;
-                menuXPos = size.width;
-                break;
-            }
-          });
-        }
-        ), transform: Matrix4.translationValues(bodyXPos, 0, 0),),
-      AnimatedContainer(
-        duration: duration,
-        transform: Matrix4.translationValues(menuXPos, 0, 0),
-      child: ProfileSideMenu(menuWidth),
-    ),]
-    ,
-    )
-    );
+                  switch (_menuStatus) {
+                    case MenuStatus.opened:
+                      bodyXPos = -menuWidth;
+                      menuXPos = size.width - menuWidth;
+                      break;
+                    case MenuStatus.closed:
+                      bodyXPos = 0;
+                      menuXPos = size.width;
+                      break;
+                  }
+                });
+              }),
+              transform: Matrix4.translationValues(bodyXPos, 0, 0),
+            ),
+            AnimatedContainer(
+              duration: duration,
+              transform: Matrix4.translationValues(menuXPos, 0, 0),
+              child: ProfileSideMenu(menuWidth),
+            ),
+          ],
+        ));
   }
 }
 
