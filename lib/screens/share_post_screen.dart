e88ffd2128fdd962810c00748efb5b139caeca97
kimjongseong <miaer456@naver.com>
2022-04-16 14:31:21 +0900

    Instagram 10

--- lib/screens/share_post_screen.dart
+++ lib/screens/share_post_screen.dart
@@ -5,10 +5,25 @@ import 'package:flutter/material.dart';
 class SharePostScreen extends StatelessWidget {
   final File imageFile;
   final String postKey;
-  const SharePostScreen(this.imageFile,{Key key,@required this.postKey}) : super(key: key);
+
+  const SharePostScreen(this.imageFile, {Key key, @required this.postKey})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Image.file(imageFile);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('New Post'),
+        actions: [
+          FlatButton(
+              onPressed: () {},
+              child: Text(
+                "Share",
+                textScaleFactor: 1.4,
+                style: TextStyle(color: Colors.blue),
+              ))
+        ],
+      ),
+    body: Image.file(imageFile));
   }
 }
