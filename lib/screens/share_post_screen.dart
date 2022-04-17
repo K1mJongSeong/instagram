7689989dbdd14ff4506b116410a9bc2ed1922f60
kimjongseong <miaer456@naver.com>
2022-04-17 21:52:14 +0900

    Instagram 10

--- lib/screens/share_post_screen.dart
+++ lib/screens/share_post_screen.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_project/constants/common_size.dart';
+import 'package:flutter_project/constants/screen_size.dart';
 
 class SharePostScreen extends StatelessWidget {
   final File imageFile;
@@ -12,18 +14,35 @@ class SharePostScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('New Post'),
-        actions: [
-          FlatButton(
-              onPressed: () {},
-              child: Text(
-                "Share",
-                textScaleFactor: 1.4,
-                style: TextStyle(color: Colors.blue),
-              ))
-        ],
-      ),
-    body: Image.file(imageFile));
+        appBar: AppBar(
+          title: Text('New Post'),
+          actions: [
+            FlatButton(
+                onPressed: () {},
+                child: Text(
+                  "Share",
+                  textScaleFactor: 1.4,
+                  style: TextStyle(color: Colors.blue),
+                ))
+          ],
+        ),
+        body: ListView(
+          children: [
+            ListTile(
+              contentPadding: EdgeInsets.symmetric(
+                  vertical: common_gap, horizontal: common_gap),
+              leading: Image.file(
+                imageFile,
+                width: size.width / 6,
+                height: size.height / 6,
+                fit: BoxFit.cover,
+              ),
+              title: TextField(
+                decoration: InputDecoration(
+                    hintText: 'Write a caption..', border: InputBorder.none),
+              ),
+            ),
+          ],
+        ));
   }
 }
